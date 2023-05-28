f1954d40c394a017cfa67b7c17afa9afb24a13d3
jewelrana3 <juyelrana7752@gmail.com>
2023-05-28 23:13:44 +0600

    last

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cnbwwnw.mongodb.net/?retryWrites=true&w=majority`;
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cnbwwnw.mongodb.net/?retryWrites=true&w=majority`
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -54,6 +56,13 @@ async function run() {
       const id = req.body;
       console.log(id);
       const result = await cartCollection.insertOne(id);
+      res.send(result)
+    });
+
+    app.delete('/carts/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query);
       res.send(result)
     })
 
